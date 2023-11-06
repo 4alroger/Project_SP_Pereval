b33aeb74cbd333ae2c6cfdf47267c97eeec379db
4alroger <129160214+4alroger@users.noreply.github.com>
2023-11-06 17:51:03 +0400

    Update views.py
    Доработан файл views.py. Добавлены классы фильтрации EmailAPIView и MountDetailViewSet для выполнения заданий Спринт 2

--- Pass/pass_app/views.py
+++ Pass/pass_app/views.py
@@ -2,7 +2,8 @@ import django_filters
 from rest_framework.response import Response
 
 from .serializers import *
-from rest_framework import viewsets, status
+from rest_framework import viewsets, status, generics
+from django.http import JsonResponse
 
 
 class CoordsViewSet(viewsets.ModelViewSet):
@@ -26,6 +27,11 @@ class MountViewSet(viewsets.ModelViewSet):
     filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
     filterset_fields = ["user__email"]
 
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+
     def create(self, request, *args, **kwargs):
         serializer = MountSerializer(data=request.data)
         if serializer.is_valid():
@@ -48,6 +54,7 @@ class MountViewSet(viewsets.ModelViewSet):
                 'id': None,
             })
 
+    # редактирование данных перевала, при статусе "new" без изменений данных пользователя
     def partial_update(self, request, *args, **kwargs):
         record = self.get_object()
         if record.status == 'new':
@@ -74,3 +81,44 @@ class MountViewSet(viewsets.ModelViewSet):
                     'message': f'При данном статусе: {record.get_status_display()}, данные изменить нельзя!'
                 }
             )
+
+"""Класс работы с БД для второго спринта: извлечение и редактирование перевала"""
+class MountDetailViewSet(viewsets.ModelViewSet):
+    queryset = Mount.objects.all()
+    serializer_class = MountDetailSerializer
+
+    def update(self, request, *args, **kwargs):
+        """
+        Переопределение метода update(PATCH)
+        """
+        pk = kwargs.get("pk", None)
+
+        try:
+            instance = Mount.objects.get(pk=pk)
+        except:
+            return Response({"error": "Такого перевала не существует"}, status=400)
+
+        if instance.status != "N":
+            return Response({"message": "Перевал на модерации, вы не можете его изменить",
+                             "state": 0}, status=400)
+        else:
+            serializer = MountDetailSerializer(data=request.data, instance=instance)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response({"state": 1}, status=200)
+
+
+""" Список данных обо всех перевалах, которые пользователь с почтой <email> отправил на сервер """
+class EmailAPIView(generics.ListAPIView):
+    serializer_class = AuthEmailMountSerializer
+    def get(self, request, *args, **kwargs):
+        email = kwargs.get('email', None)
+        if Mount.objects.filter(user__email=email):
+            data = AuthEmailMountSerializer(Mount.objects.filter(user__email=email), many=True).data
+            api_status = status.HTTP_200_OK
+        else:
+            data = {
+                'message': f'Не существует пользователя с таким email - {email}'
+            }
+            api_status = 404
+        return JsonResponse(data, status=api_status, safe=False)
