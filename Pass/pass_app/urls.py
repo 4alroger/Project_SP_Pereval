9cd7852f7f74feb865ff86fa5bebf472e9d9819f
4alroger <129160214+4alroger@users.noreply.github.com>
2023-11-06 17:58:52 +0400

    Update pass_app/urls.py
    Доработан файл ulrl.py для вывода значений по Спринт 2 после изменений в views.py и serializers.py

--- Pass/pass_app/urls.py
+++ Pass/pass_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 #from pass_app import views
-from .views import MountViewSet, CoordsViewSet, PhotoViewSet, LevelViewSet
+from .views import MountViewSet, CoordsViewSet, PhotoViewSet, LevelViewSet, MountDetailViewSet, EmailAPIView
 from rest_framework import routers
 
 router = routers.DefaultRouter()
@@ -11,5 +11,9 @@ router.register(r'level', LevelViewSet, basename='levels')
 
 
 urlpatterns = [
+    # Sprint 1
     path('submitData/', include(router.urls)),
+    # Sprint 2
+    path('submitData/<int:pk>/', MountDetailViewSet.as_view({'get': 'retrieve', 'patch': 'partial_update'})),
+    path('submitData/user__email=<str:email>', EmailAPIView.as_view()),
 ]
